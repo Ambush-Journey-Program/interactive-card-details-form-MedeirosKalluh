af24ab33dd7bca1bf3238a9298cc720fe6bfbf5b
Lucas Medeiros <lucaspontoe@hotmail.com>
2022-12-22 15:03:06 -0300

    Fix the limit of number in the form adjust de JS code for better reading

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const formNameEl = document.querySelector("#form__name")
-const formNumberEl = document.getElementById("form__number")
-const formYearEl = document.getElementById("form-yy")
-const formMonthEl = document.getElementById("form-mm")
-const formCodeEl = document.getElementById("form__code")
-
 formNameEl.addEventListener("input", function setCardName(event){
     document.querySelector('#card-name').textContent = event.target.value;
 })
 
+const formNumberEl = document.getElementById("form__number")
 formNumberEl.addEventListener("input", function setCardNumber(event){
     document.querySelector('#card-number').textContent = event.target.value;
 })
 
+const formYearEl = document.getElementById("form-yy")
 formYearEl.addEventListener("input", function setYear(event){
     document.querySelector(`#card-YY`).textContent = event.target.value;
 })
 
+const formMonthEl = document.getElementById("form-mm")
 formMonthEl.addEventListener("input", function setMonth(event){
     document.querySelector('#card-MM').textContent = event.target.value;
 })
 
+const formCodeEl = document.getElementById("form__code")
 formCodeEl.addEventListener("input", function setCardCode(event){
     document.querySelector('#card-code').textContent = event.target.value;
 })
